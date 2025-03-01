49865e9ac8013fa7d3873d20922805562a873d7c
SquishyMona <kotonefemc@gmail.com>
2025-03-01 13:53:48 -0500

    typo

---
+++ commands/admin/stopwatchchannel.js
@@ -0,0 +1,22 @@
+import { SlashCommandBuilder } from "discord.js";
+import { stopChannels } from "../../lib/gcal/stopchannels.js";
+
+export const data = new SlashCommandBuilder()
+	.setName("stopwatchchannel")
+	.setDescription("Stop watching a channel for this server.")
+	.addStringOption(option =>
+		option.setName("channel_id")
+			.setDescription("The channel_id you want to stop watching.")
+			.setRequired(true)
+	)
+	.addStringOption(option =>
+		option.setName("resource_id")
+			.setDescription("The recource_id you want to stop.")
+			.setRequired(true)
+	);
+
+export const execute = async (interaction) => {
+	const channelId = interaction.options.getString('channel_id');
+	const resourceId = interaction.options.getString('resource_id');
+	await stopChannels(interaction.user.id, channelId, resourceId);
+}
