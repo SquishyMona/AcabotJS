d98c20528bc58fa6147c7c397a7e328e011e2c62
SquishyMona <kotonefemc@gmail.com>
2025-02-28 21:30:17 -0500

    new changes again

--- commands/calendar/linkaccount.js
+++ commands/calendar/linkaccount.js
@@ -1,20 +1,25 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { MessageFlags, SlashCommandBuilder } from 'discord.js';
 import { linkGoogleAccount } from '../../lib/gcal/auth.js';
+import { auth } from '../../events/ready.js';
+const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 
 const data = new SlashCommandBuilder()
 	.setName('linkaccount')
 	.setDescription('Link your Google Calendar account');
 
 const execute = async (interaction) => {
-	await interaction.deferReply();
-	interaction.followUp('Attempting to link your Google Calendar account...');
+	await interaction.deferReply({ flags: MessageFlags.Ephemeral });
 	const discordUserId = interaction.user.id;
+	if (await auth.isGuildLoggedIn('google', discordUserId)) {
+		return await interaction.followUp({ content: 'Your Google Calendar account is already linked.', flags: MessageFlags.Ephemeral });
+	}
+	interaction.followUp({ content:'Attempting to link your Google Calendar account...', flags: MessageFlags.Ephemeral});
 	try {
-		await linkGoogleAccount(discordUserId);
-		await interaction.followUp('Your Google Calendar account has been linked successfully!');
+		const authURL = await auth.generateAuthURL('google', discordUserId, SCOPES);
+		await interaction.followUp({ content: 'Follow this link to complete the process: ' + authURL, flags: MessageFlags.Ephemeral });
 	} catch (error) {
 		console.error(error);
-		await interaction.followUp('There was an error linking your Google Calendar account.');
+		await interaction.followUp({ content: 'There was an error linking your Google Calendar account.', flags: MessageFlags.Ephemeral });
 	}
 };
 
--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,27 @@
 import { Events } from 'discord.js';
 import { incrementalSync } from '../lib/gcal/incrementalSync.js';
 import { getUpcoming } from '../lib/gcal/getUpcoming.js';
+import AuthConnect from 'authconnect-djs';
+import Firestore from '@google-cloud/firestore';
 
 export const name = Events.ClientReady;
 export const once = true;
+
+export let auth;
+
 export const execute = async (client) => {
 	console.log(`Ready! Logged in as ${client.user.tag}`);
 
+	const db = new Firestore.Firestore({ projectId: 'acabotjs', keyFilename: `${process.cwd()}/cloud/serviceaccount.json` });
+	auth = new AuthConnect({
+		google: {
+			clientId: process.env.AUTH_CONNECT_CLIENTID,
+			clientSecret: process.env.AUTH_CONNECT_CLIENTSECRET,
+		}
+	});
+
+	auth.useFirestoreDataHandlers(db, 'auth');
+	
 	setInterval(incrementalSync, 1000 * 60 * 5);
 	setInterval(async () => await getUpcoming(client), 1000 * 60);
 };
