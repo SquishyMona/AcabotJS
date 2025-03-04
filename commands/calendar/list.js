9c9954e4e729ca222321fb576b09c29998e82edc
Ian Cioppa <ian.cioppa@gmail.com>
2025-03-04 12:54:09 -0500

    listing command

--- commands/calendar/list.js
+++ commands/calendar/list.js
@@ -27,7 +27,36 @@ const data = new SlashCommandBuilder()
 					.setDescription('If true, only you will be able to see the bots response')
 					.setRequired(false),
 			),
-	);
+	)
+	.addSubcommand(subcommand =>
+		subcommand.setName('events')
+			.setDescription('Get events from calendars linked to this server.')
+			.addStringOption(option =>
+				option.setName('calendar')
+					.setDescription('The calendar to list events from')
+					.setRequired(true)
+					.setAutocomplete(true),
+			)
+			.addStringOption(option =>
+				option.setName('start')
+					.setDescription('The start date to list events from')
+					.setRequired(false),
+			)
+			.addStringOption(option =>
+				option.setName('end')
+					.setDescription('The end date to list events from')
+					.setRequired(false),
+			)
+			.addBooleanOption(option =>
+				option.setName('hideresponse')
+					.setDescription('If true, only you will be able to see the bots response')
+					.setRequired(false),
+			)
+		);
+
+const autocomplete = async (interaction) => {
+	await linkedCalendarAutocomplete(interaction);
+}
 
 const execute = async (interaction) => {
 	if (interaction.options.getBoolean('hideresponse')) await interaction.deferReply({ flags: MessageFlags.Ephemeral }); else await interaction.deferReply();
@@ -35,24 +64,28 @@ const execute = async (interaction) => {
 	if (interaction.options.getSubcommand() === 'fredoniaevents') {
 		await listFredoniaEvents(interaction, interaction.options.getString('filter'), interaction.options.getString('date'));
 		return;
+	} else {
+		const calendarId = interaction.options.getString('calendar');
+		const startDate = interaction.options.getString('start');
+		const endDate = interaction.options.getString('end');
+		await interaction.followUp({ content: 'Fetching events...', flags: MessageFlags.Ephemeral});
+		const results = await listEvents(interaction.member.id, calendarId).catch(async (error) => {
+			console.error(error);
+			await interaction.followUp('There was an error fetching events. Make sure you\'ve linked your Google Account using /linkaccount, then try again.');
+		});
+		console.log(results);
+
+		const embed = new EmbedBuilder()
+			.setTitle('Events')
+			.setColor(0x00AE86)
+			.setDescription('List of events in your calendar');
+
+		results.forEach((event) => {
+			embed.addFields([{ name: event.summary, value: `Start: ${event.start.dateTime}\nEnd: ${event.end.dateTime}` }]);
+		});
+		await interaction.followUp('Events fetched successfully!');
+		await interaction.channel.send({ embeds: [embed] });
 	}
-	await interaction.followUp('Authorizing and fetching events...');
-	const results = await listEvents(interaction.member.id).catch(async (error) => {
-		console.error(error);
-		await interaction.followUp('There was an error fetching events. Make sure you\'ve linked your Google Account using /linkaccount, then try again.');
-	});
-	console.log(results);
-
-	const embed = new EmbedBuilder()
-		.setTitle('Events')
-		.setColor(0x00AE86)
-		.setDescription('List of events in your calendar');
-
-	results.forEach((event) => {
-		embed.addFields([{ name: event.summary, value: `Start: ${event.start.dateTime}\nEnd: ${event.end.dateTime}` }]);
-	});
-	await interaction.followUp('Events fetched successfully!');
-	await interaction.channel.send({ embeds: [embed] });
 };
 
 export { data, execute };
