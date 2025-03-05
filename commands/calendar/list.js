755d3170c9dcd0f66ace39808da448fc84db3a52
SquishyMona <kotonefemc@gmail.com>
2025-03-04 21:03:48 -0500

    forgot a small thing

--- commands/calendar/list.js
+++ commands/calendar/list.js
@@ -89,7 +89,7 @@ export const execute = async (interaction) => {
 
 		results.forEach((event) => {
 			const timezoneOffset = (new Date().getTimezoneOffset() / 60) * (1000 * 60 * 60);
-			const date = event.start.dateTime ? new Date(event.start.dateTime).getTime() : new Date(new Date(event.start.date).getTime() + timezoneOffset);
+			const date = event.start.dateTime ? new Date(event.start.dateTime).getTime() : new Date(new Date(event.start.date).getTime() + timezoneOffset).getTime();
 			embed.addFields([{ name: event.summary, value: `Date<t:${date}:D>\nStart: <t${new Date(event.start.dateTime).getTime()}\nEnd: ${new Date(event.end.dateTime.getTime())}` }]);
 		});
 		await interaction.followUp('Events fetched successfully!');
